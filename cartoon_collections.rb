7127e6751a6298f6fc2d41c869691251b5c207fe
vy vo <tuonggvy.vo@gmail.com>
2019-11-10 05:09:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-  answer = false
-  calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
-  end
-  answer
+ 
 end
 
 def find_the_cheese(list)
