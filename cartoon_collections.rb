fdd807174cc69667c4d55313984a172bfeaf5b68
vy vo <tuonggvy.vo@gmail.com>
2019-11-10 05:05:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(planeteer_calls)
   new_array = []
   calls.collect do |call|
-    new_array << call.capitalize!
+    new_array << #{call.capitalize!}
   end
   new_array
 end
